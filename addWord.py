eb1c34a4fd318a2a5a85139b442b4e0a73f1089b
giovanni <144000giovanni@gmail.com>
2022-10-29 10:30:30 +1300

    'update'

---
+++ addWord.py
@@ -0,0 +1,17 @@
+import json
+
+def add(word):
+  f = open("wordList.json", "r")
+  jsondata = json.loads(f.read())
+  f.close()
+  if word not in jsondata["items"]:
+    jsondata["items"].append(word)
+    with open("wordList.json", "w") as outfile:
+      json.dump(jsondata, outfile, indent=4, sort_keys=True )
+
+
+while True:
+  word = input("add a word: ")
+  confirmSpelling = input("spelling correct for '" + word + "'?")
+  if confirmSpelling.lower()=="y":
+    add(word)
---
+++ app.js
@@ -0,0 +1,40 @@
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function sayTheWord(word){
+	  // Check if Speech Synthesis supported
+  console.log("sayTheWord: " + word);
+  if ("speechSynthesis" in window) {
+    const msg = new SpeechSynthesisUtterance();
+    msg.text = "Spell " + word;
+    window.speechSynthesis.speak(msg);
+  } else {
+    // Speech Synthesis Not Supported
+    alert("Sorry, your browser doesn't support text to speech!");
+  }
+}
+function loadWords(data){
+	var containerList = document.getElementById("wordtoGuess");
+	var max = data["items"].length;
+	
+	var randomNumber = getRandomInt(max);
+	console.log("max: " + max);
+	var word = data["items"][randomNumber];
+	console.log(word);
+	document.getElementById("answer").style.display = "none";
+    document.getElementById("answer").innerHTML = word;
+
+	sayTheWord(word);
+	
+}
+
+function fetchWords(filename){
+	
+fetch(filename)
+  .then((response) => response.json())
+  .then(loadWords);
+  
+}
+
